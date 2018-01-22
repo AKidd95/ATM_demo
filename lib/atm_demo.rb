e3a82d2e1dcc0b5ba24bfcbdb35197e630c43c6f
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-01-22 18:47:15 +0100

    added instance double and withdraw method

--- lib/atm_demo.rb
+++ lib/atm_demo.rb
@@ -7,8 +7,15 @@ class ATM
     @funds = 1000
   end
 
-  def withdraw(number)
-    @funds -= number
+  def withdraw(amount, account)
+    case
+    when amount > account.balance
+      return
+    else
+      @funds -= amount
+      account.balance = account.balance - amount
+      {status: true, message: 'success', date: Date.today, amount: amount}
+    end
   end
 
 end
