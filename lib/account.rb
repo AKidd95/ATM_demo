7588c9efe25dcd9e7f4e1fc9813dd62fc7861318
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-01-23 13:51:46 +0100

    added balance & pin code specs

--- lib/account.rb
+++ lib/account.rb
@@ -1,11 +1,21 @@
 class Account
   require 'date'
+  attr_accessor :pin_code, :balance
 
   STANDARD_VALIDITY_YRS = 5
+
+  def initialize()
+    @pin_code = generate_pin
+    @balance = 0
+  end
 
   def exp_date
     Date.today.next_year(STANDARD_VALIDITY_YRS).strftime("%m/%y")
   end
 
+  private
+  def generate_pin
+    rand(1000..9999)
+  end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,15 +2,18 @@ require './lib/account'
 
 describe Account do
 
-  it 'check length of number' do
-    number = rand(1000..9999)
-    number_length = Math.log10(number).to_i + 1
-    expect(number_length = 4)
+  it 'is expected to have a 4 digit pin number' do
+    pin_length = Math.log10(subject.pin_code).to_i + 1
+    expect(pin_length).to eq 4
   end
 
   it 'is expected to have an expiry date on initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
+  end
+
+  it 'is expected to have a balance of 0' do
+    expect(subject.balance).to eq 0
   end
 
 
